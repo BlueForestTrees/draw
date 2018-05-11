eff23cb4fa4aa7999af62a9424426c35def5c2b5
slim <smedini@gmail.com>
2018-05-11 10:31:28 +0200

    zoom et pan

--- src/main/vue/panel/Zoom.vue
+++ src/main/vue/panel/Zoom.vue
@@ -1,7 +1,6 @@
 <template>
     <span>
-        <v-btn flat @click="reinitZoom">Zoom 1 : 1</v-btn>
-        <v-btn flat @click="reinitPan">Pan 1 : 1</v-btn>
+        <v-btn flat @click="reinitZoom">1 : 1</v-btn>
         <v-btn-toggle v-model="nav.zoomSide">
             <v-btn flat><v-icon>zoom_in</v-icon></v-btn>
             <v-btn flat><v-icon>zoom_out</v-icon></v-btn>
@@ -21,8 +20,6 @@
         methods: {
             reinitZoom() {
                 this.film.f.zoom = 1;
-            },
-            reinitPan() {
                 this.film.f.panx = 0;
                 this.film.f.pany = 0;
             }
